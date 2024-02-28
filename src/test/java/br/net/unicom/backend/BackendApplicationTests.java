3e06fcfd0b78048add3006df6997b32b4609ca09
Pedro <kapoty1@gmail.com>
2024-02-28 09:27:55 -0300

    initial

---
+++ src/test/java/br/net/unicom/backend/BackendApplicationTests.java
@@ -0,0 +1,15 @@
+package br.net.unicom.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads(){
+	}
+
+}
